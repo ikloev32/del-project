d0700f1ff93597405d424f25a622f925ad2c2b41
이동주 <ikloev32@naver.com>
2024-04-25 20:29:47 +0900

    시나리오js => ts

---
+++ src/module/Chapter.ts
@@ -0,0 +1,12 @@
+const Chapter = () => {
+  const sinarioList = []
+  const sinarioIndex = 0
+
+
+  const addSinarioEvents = (sinario) => {
+    sinarioList.push(sinario)
+  }
+
+}
+
+export default Chapter
---
+++ src/module/Sinario.ts
@@ -0,0 +1,12 @@
+const Sinario = ({
+  name,
+  text,
+  audio,
+  background,
+  delay = 1000,
+}) => {
+
+  const setEvents = () => {
+
+  }
+}
